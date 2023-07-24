126d3cb1b897316d8a35749dcb5e8ce1d58a2fdf
scivision <scivision@users.noreply.github.com>
2023-07-24 15:03:55 -0400

    cmake template updates

--- cmake/abi_check/main.c
+++ cmake/abi_check/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int addone(int);
+#include "abi_check.h"
 
 
 int main(void) {
 
   if(addone(2) != 3) {
     fprintf(stderr, "2 + 1 != %d", addone(2));
+    return EXIT_FAILURE;
+  }
+
+  if(addtwo(2) != 4) {
+    fprintf(stderr, "2 + 2 != %d", addtwo(2));
     return EXIT_FAILURE;
   }
 
