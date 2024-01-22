e803301f715fa8212540e1bf2adc3b4a4220cfc2
scivision <scivision@users.noreply.github.com>
2024-01-21 19:10:21 -0500

    simplify/correct cmake, use presets
    git submodule: allow offline

--- options.cmake
+++ options.cmake
@@ -2,14 +2,6 @@ if(DEFINED SCALAPACK_IS_TOP)
   set(SCALAPACK_IS_TOP_LEVEL ${SCALAPACK_IS_TOP})
 endif()
 
-if(local)
-  get_filename_component(local ${local} ABSOLUTE)
-
-  if(NOT IS_DIRECTORY ${local})
-    message(FATAL_ERROR "Local directory ${local} does not exist")
-  endif()
-endif()
-
 # --- other options
 
 option(BUILD_SINGLE "Build single precision real" ON)
@@ -27,12 +19,12 @@ endif()
 
 option(find_lapack "find LAPACK" on)
 
+option(${PROJECT_NAME}_BUILD_TESTING "Build tests" ${SCALAPACK_IS_TOP_LEVEL})
+
 # used with Git submodule to avoid rechecking each build for submodule changes
 # for developers who switch submodule commits, need a fresh build of entire project.
 set(FETCHCONTENT_UPDATES_DISCONNECTED true)
 set_property(DIRECTORY PROPERTY EP_UPDATE_DISCONNECTED true)
-
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/cmake)
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
