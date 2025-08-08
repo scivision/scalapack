d8320f4743b450b5eb5bddcff19b1edf61703c2f
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-07 20:16:24 -0400

    allow consumption as an archive from other projects

--- cmake/GitSubmodule.cmake
+++ cmake/GitSubmodule.cmake
@@ -1,28 +1,37 @@
-# else it's an offline archive
-if(IS_DIRECTORY ${PROJECT_SOURCE_DIR}/.git)
-  find_package(Git REQUIRED)
-endif()
+find_package(Git REQUIRED)
+
 
 function(git_submodule submod_dir)
 # get/update Git submodule directory to CMake, assuming the
 # Git submodule directory is a CMake project.
 
-# EXISTS, do not use IS_DIRECTORY as in submodule .git is a file not a directory
-if(NOT EXISTS ${PROJECT_SOURCE_DIR}/.git)
-  message(DEBUG "${PROJECT_SOURCE_DIR} is not a Git repository, skipping submodule ${submod_dir}")
-  return()
-endif()
-
 if(EXISTS ${submod_dir}/CMakeLists.txt)
   return()
 endif()
 
-execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${submod_dir}
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-RESULT_VARIABLE err)
+set(CMAKE_EXECUTE_PROCESS_COMMAND_ECHO STDOUT)
 
-if(NOT err EQUAL 0)
-  message(FATAL_ERROR "${submod_dir} Git submodule failed to retrieve.")
+# EXISTS, do not use IS_DIRECTORY as in submodule .git is a file not a directory
+if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
+  message(DEBUG "${PROJECT_SOURCE_DIR} is a Git repository, updating submodule ${submod_dir}")
+  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${submod_dir}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND_ERROR_IS_FATAL ANY
+    )
+else()
+  message(DEBUG "${PROJECT_SOURCE_DIR} is from an archive or otherwise not a Git repository. Getting .gitmodules info to clone")
+  execute_process(COMMAND ${GIT_EXECUTABLE} config --file ${PROJECT_SOURCE_DIR}/.gitmodules --get submodule.scalapack.url
+                  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                  OUTPUT_VARIABLE submod_url
+                  OUTPUT_STRIP_TRAILING_WHITESPACE
+                  COMMAND_ERROR_IS_FATAL ANY
+                  )
+  # Some platforms like Windows, Git might refuse to clone into a totally empty existing directory
+  # this is a known technique, to "git clone" from that empty directory to make it work.
+  execute_process(COMMAND ${GIT_EXECUTABLE} clone ${submod_url} ${submod_dir}
+                  WORKING_DIRECTORY ${submod_dir}
+                  COMMAND_ERROR_IS_FATAL ANY
+                  )
 endif()
 
 endfunction()
